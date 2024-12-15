3db6048e2bb11a3c546a30fed47ed169b4c23850
Maryam Raheem <maryamraheem09876@gmail.com>
2024-12-15 20:37:55 +0500

    Better Logging with volume mounts

--- backend/utils/logger.py
+++ backend/utils/logger.py
@@ -1,8 +1,15 @@
 import logging
+import os
 from datetime import datetime
 
+if os.path.exists("./logs") == False:
+    os.mkdir("./logs")
+
+current_time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+fileName = f"./logs/app-{current_time}.log"
+
 # Set up logging
-logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s - %(message)s')
+logging.basicConfig(filename=fileName, level=logging.INFO, format='%(asctime)s - %(message)s')
 
 def log_action(action_type, details):
     logging.info(f"{action_type}: {details}")
